c9b2d8d861da602b00e4cfe52e93183f6493ed4e
JakenCool <409265705@qq.com>
2017-05-21 23:39:24 +0800

    .

--- src/Test.java
+++ src/Test.java
@@ -25,7 +25,7 @@ public class Test {
     	System.out.println();
     	
     	Passenger.reserveFlight();
-    	
+    	Passenger.unsubscribedFlight();
       
 //        Admin.deleteFlight()
 //        Admin.create_newAdmin();
